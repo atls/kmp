3f4adaff1be1d010005d1271d758dd1b5c91cad0
kirill ivanov <iwwwanowwwwwww@yandex.ru>
2025-04-08 19:02:14 +0300

    feat(common): dublicate bump command to other packages

--- multiplatform/build.gradle.kts
+++ multiplatform/build.gradle.kts
@@ -26,3 +26,22 @@ gradlePlugin {
         }
     }
 }
+
+tasks.register("bumpPatchVersion") {
+	doNotTrackState("Modifies build files")
+
+    doLast {
+        val (major, minor, patch) = version.toString().split(".").map { it.toInt() }
+        val newVersion = "$major.$minor.${patch + 1}"
+
+        val buildFile = file("build.gradle.kts")
+        val content = buildFile.readText()
+            .replace(
+                Regex("version\\s*=\\s*[\"']$version[\"']"), 
+                "version = \"$newVersion\""
+            )
+
+        buildFile.writeText(content)
+        println("Version updated from $version to $newVersion")
+    }
+}
