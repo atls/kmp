b1f5ee305efb2994376a2882898f8046e07d5c28
kirill ivanov <iwwwanowwwwwww@yandex.ru>
2025-04-09 14:50:23 +0300

    refactor(common): move patch version command to root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     `maven-publish`
     `kotlin-dsl`
+    kotlin("multiplatform") version "2.1.0"
 }
 
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     google()
@@ -17,6 +18,31 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     implementation("com.android.tools.build:gradle:8.9.0")
     // implementation("org.jetbrains.multiplatform:multiplatform-gradle-plugin:1.7.0")
+}
+
+tasks.register("bumpPatchVersion") {
+	doNotTrackState("Modifies build files")
+
+    doLast {
+        val (major, minor, patch) = version.toString().split(".").map { it.toInt() }
+        val newVersion = "$major.$minor.${patch + 1}"
+
+        val buildFile = file("build.gradle.kts")
+        val content = buildFile.readText()
+            .replace(
+                Regex("version\\s*=\\s*[\"']$version[\"']"), 
+                "version = \"$newVersion\""
+            )
+
+        buildFile.writeText(content)
+        println("Version updated from $version to $newVersion")
+    }
+}
+
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    if (publication.name == "pluginMaven") {
+        enabled = false
+    }
 }
 
 tasks.register("bumpAllVersions") {
