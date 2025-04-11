c6978d24f71123a1d7001137861b6832e6e96807
kirill ivanov <iwwwanowwwwwww@yandex.ru>
2025-04-11 13:21:38 +0300

    refactor(common): mv publishing logic into publishing

--- publishing.gradle.kts
+++ publishing.gradle.kts
@@ -4,11 +4,24 @@ plugins.withId("maven-publish") {
             maven {
                 name = "GitHubPackages"
                 url = uri("https://maven.pkg.github.com/atls/kmp")
+				version = project.version.toString()
                 credentials {
                     username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
                     password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN")
                 }
             }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            stagingProfileId.set(project.findProperty("stagingProfileId")?.toString() ?: "")
+            username.set(project.findProperty("sonataUsername")?.toString())
+            password.set(project.findProperty("sonataPassword")?.toString())
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
         }
     }
 }
